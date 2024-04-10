fac72ad0b2377b6146dde4e1c58e88b9dbdc480d
DINAMOW <meemoo102039@gmail.com>
2024-04-10 16:29:06 +0200

    fix: remove comments from setup

--- 0x01-loginService/setup.bash
+++ 0x01-loginService/setup.bash
@@ -2,19 +2,19 @@
 
 read -s -p "Enter the database password: " db_password
 echo -e "\n"
-# sudo apt update
-# sudo apt upgrade -y
-# sudo apt install mysql-server
-# sudo apt install python3 python3-pip
-# sudo apt install python3-venv
+sudo apt update
+sudo apt upgrade -y
+sudo apt install mysql-server
+sudo apt install python3 python3-pip
+sudo apt install python3-venv
 echo 'CREATE DATABASE IF NOT EXISTS `Djangoapp`' | sudo mysql
 echo "CREATE USER IF NOT EXISTS 'Djangouser'@'localhost' IDENTIFIED BY '$db_password'" | sudo mysql
 echo "GRANT ALL PRIVILEGES ON Djangoapp.* TO 'Djangouser'@'localhost'" | sudo mysql
 echo 'FLUSH PRIVILEGES' | sudo mysql
 echo "password=$db_password" > .env
-# python3 -m venv env
+python3 -m venv env
 source env/bin/activate
-# pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
