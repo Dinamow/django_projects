c911726811e551dbb6e4c0064882d636b402ebcf
DINAMOW <meemoo102039@gmail.com>
2024-04-12 15:42:19 +0200

    testing: trying endpoint with some views for testing the user class

--- 0x01-loginService/users/urls.py
+++ 0x01-loginService/users/urls.py
@@ -4,6 +4,5 @@ from users.views import *
 
 urlpatterns = [
     path('create_user/', create_user, name='create_user'),
-    path('users_count/', users_count, name='users_count'),
     path('all_users/', all_users, name='all_users'),
 ]
--- 0x01-loginService/users/views.py
+++ 0x01-loginService/users/views.py
@@ -1,3 +1,17 @@
+from django.http import JsonResponse
+from users.models import Users
 from django.shortcuts import render
 
 # Create your views here.
+def create_user(request):
+    Users.objects.create(username="test", password="test",
+                             email="meemoo102039@gmail.com", phone="1234567890",
+                             address="test", city="test", state="test",
+                             country="test", zip="123456")
+    return JsonResponse({"status": "success"})
+
+
+def all_users(request):
+    users = Users.objects.all()
+    return JsonResponse({"count": Users.objects.count(),
+                         "users": list(users.values())})
