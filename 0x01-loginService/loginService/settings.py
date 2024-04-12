4e9f662f841fa6087c3ebe82c6440a9f5409b343
DINAMOW <meemoo102039@gmail.com>
2024-04-12 10:14:48 +0200

    improvment: add my user application to the project

--- 0x01-loginService/loginService/settings.py
+++ 0x01-loginService/loginService/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -85,6 +86,9 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
+        'TEST': {
+            'NAME': 'Djangoapp_test',
         }
     }
 }
