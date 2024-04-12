8ecbc244011cc90998bcb123dcffe889c28fa028
DINAMOW <meemoo102039@gmail.com>
2024-04-13 00:24:46 +0200

    fix: change uuid4 to uuid4() to return the value of the function not the function itself

--- 0x01-loginService/users/models.py
+++ 0x01-loginService/users/models.py
@@ -9,7 +9,7 @@ class Users(models.Model):
     phone = models.CharField(max_length=50, unique=True)
     session_token = models.CharField(max_length=50, default=0)
     reset_token = models.CharField(max_length=50, default=0)
-    activation_token = models.CharField(max_length=50, default=str(uuid4))
+    activation_token = models.CharField(max_length=50, default=str(uuid4()))
     activated = models.BooleanField(default=False)
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
