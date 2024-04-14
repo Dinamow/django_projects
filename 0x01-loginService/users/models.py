6f589c26d3b0de720a1e951e983812a8cb370ff3
DINAMOW <meemoo102039@gmail.com>
2024-04-14 14:55:47 +0200

    fix: change the max char len to 255 to be able to store hashed password

--- 0x01-loginService/users/models.py
+++ 0x01-loginService/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Users(models.Model):
     username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=255)
     email = models.EmailField(max_length=50, unique=True)
     phone = models.CharField(max_length=50, unique=True)
     session_token = models.CharField(max_length=50, default=0)
