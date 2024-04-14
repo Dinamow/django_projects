3a0321769f2ec61171bf69ef30d98883bf5b60e3
DINAMOW <meemoo102039@gmail.com>
2024-04-14 14:26:35 +0200

    add: activation endpoint for activation the account, login endpoint

--- 0x01-loginService/users/urls.py
+++ 0x01-loginService/users/urls.py
@@ -5,5 +5,7 @@ from users.views import *
 urlpatterns = [
     path('', working_app, name='working_app'),
     path('signup/', create_user, name='signup'),
+    path('activate/', activate_user, name='activate'),
+    path('login/', login_user, name='login'),
     path('all_users/', all_users, name='all_users'),
 ]
