8986230d27673e21c788a5f592ebf4abe1be275a
DINAMOW <meemoo102039@gmail.com>
2024-04-16 00:21:52 +0200

    add: profile end point to get each user profile based on their username

--- 0x01-loginService/users/urls.py
+++ 0x01-loginService/users/urls.py
@@ -3,12 +3,11 @@ from users.views import *
 
 
 urlpatterns = [
-    path('', working_app, name='working_app'),
     path('signup/', create_user, name='signup'),
     path('activate/', activate_user, name='activate'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('forgot_password/', forgot_password, name='forgot_password'),
     path('reset_password/', reset_password, name='reset_password'),
-    path('all_users/', all_users, name='all_users'),
+    path('<str:username>', profile, name='profile'),
 ]
